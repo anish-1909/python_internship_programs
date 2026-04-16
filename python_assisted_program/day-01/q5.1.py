47ca48a31c59f0bb10f8d48729b30537bd42d02c
anish-1909 <anishxyz10@gmail.com>
2026-04-16 15:47:52 +0530

    Removed unwanted folders, kept only day-02

--- python_assisted_program/day-01/q5.1.py
+++
@@ -1,7 +0,0 @@
-# PROG 5.1: Hard Coding Values
-PI = 22/7
-def calculate_area(radius):
-    return PI* radius ** 2
-radius = 2
-total = calculate_area(radius)
-print(f"Area of a Circle with radius = {radius} is  {total}")
